14760857555216110be8b1fbc91355b56150d79d
Nivethan <arnive19@gmail.com>
2021-12-29 21:00:08 +0530

    Add eslint & prettier setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen text-white w-screen bg-gray-700">
+    <div className="flex flex-col justify-center h-screen text-white w-screen items-center bg-gray-700">
       <h1>initial setup</h1>
-      <button className="bg-blue-500 px-4 py-2 rounded-md text-white font-semibold ">with tailwind</button>
+      <button
+        type="button"
+        className="bg-blue-500 px-4 py-2 rounded-md text-white font-semibold "
+      >
+        with tailwind
+      </button>
     </div>
   );
 }
