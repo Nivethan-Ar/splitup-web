5a5d02df09f6d689a8bb2ea1bd0e50b0c9510226
Nivethan <arnive19@gmail.com>
2022-01-01 12:42:09 +0530

    Add Topbar component

---
+++ src/components/TopBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TopBar() {
+  return (
+    <div className="border-b-2 border-opacity-10 border-black">
+      <h2>SPLITUP logo</h2>
+    </div>
+  );
+}
+
+export default TopBar;
