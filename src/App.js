d152ffa5b0d775ef07827cbb4c798c3d507d6d51
Nivethan <arnive19@gmail.com>
2022-01-01 12:41:43 +0530

    Add routes to the app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="flex flex-col justify-center h-screen text-white w-screen items-center bg-gray-700">
-      <h1>initial setup</h1>
-      <button
-        type="button"
-        className="bg-blue-500 px-4 py-2 rounded-md text-white font-semibold "
-      >
-        with tailwind
-      </button>
+    <div>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
