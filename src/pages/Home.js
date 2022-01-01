329762302b788fcf388cd7f44b9f6867d493fe41
Nivethan <arnive19@gmail.com>
2022-01-01 12:42:30 +0530

    Add home page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Body from "../layouts/Body";
+
+function Home() {
+  return (
+    <Body>
+      <div>
+        <h1>home page</h1>
+      </div>
+    </Body>
+  );
+}
+
+export default Home;
