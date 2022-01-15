ba5d804f04ba95c67b41e474be1bfb12fe36d314
Nivethan <arnive19@gmail.com>
2022-01-15 15:18:33 +0530

    Add logo to topbar and font

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,9 +1,11 @@
 import React from "react";
+import MusicLogo from "../assets/music.png";
 
 function TopBar() {
   return (
-    <div className="border-b-2 border-opacity-10 border-black">
-      <h2>SPLITUP logo</h2>
+    <div className="flex items-center gap-2 border-b-2 border-opacity-10 pb-1 border-gray-600 ">
+      <img src={MusicLogo} alt="page logo" className="w-10 animate-pulse" />
+      <h6 className="font-semibold text-2xl font-poppins ">SPLITUP</h6>
     </div>
   );
 }
