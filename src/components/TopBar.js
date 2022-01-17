b5323a5fd7c6e0621a5332d50c20dc4895c62e8e
Nivethan <arnive19@gmail.com>
2022-01-17 12:09:06 +0530

    Add padding to top bar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import MusicLogo from "../assets/music.png";
 
 function TopBar() {
   return (
-    <div className="flex items-center gap-2 border-b-2 border-opacity-10 pb-1 border-gray-600 ">
-      <img src={MusicLogo} alt="page logo" className="w-10 animate-pulse" />
-      <h6 className="font-semibold text-2xl font-poppins ">SPLITUP</h6>
-    </div>
+    <Link to="/">
+      <div className="flex items-center gap-2 border-b-2 border-opacity-10 pb-1 pt-4 border-gray-600 ">
+        <img src={MusicLogo} alt="page logo" className="w-10 animate-pulse" />
+        <h6 className="font-semibold text-2xl font-poppins ">SPLITUP</h6>
+      </div>
+    </Link>
   );
 }
 
