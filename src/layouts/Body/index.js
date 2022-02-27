e8918ac02b11cbd2dd9426f7e3c88b6a5b6af010
Nivethan <arnive19@gmail.com>
2022-02-27 14:28:49 +0530

    Add video bg with responsiveness

--- src/layouts/Body/index.js
+++ src/layouts/Body/index.js
@@ -1,11 +1,19 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import TopBar from "../../components/TopBar";
+import bgVideo from "../../assets/videos/bg2.mp4";
 
 function Body({ children }) {
   return (
-    <div className="flex flex-col justify-center h-screen text-white w-screen items-center">
-      <div className="bg-white bg-opacity-80 backdrop-blur-sm backdrop-filter bg-clip-padding border border-gray-400 shadow-xl w-10/12 h-4/5 rounded-3xl text-slate-800 px-8 py-2">
+    <div className="relative flex flex-col justify-center h-screen text-white w-screen items-center overflow-hidden">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute -z-10 md:min-w-[210%] lg:min-w-[180%] xl:min-w-[160%] 2xl:min-w-[140%] min-h-full">
+        <source src={bgVideo} type="video/mp4" />
+      </video>
+      <div className="bg-white/10 bg-opacity- backdrop-blur-lg bg-clip-padding border-4 border-white/40 shadow-xl w-11/12 h-5/6 rounded-3xl text-slate-800 px-8 py-2">
         <TopBar />
         {children}
       </div>
