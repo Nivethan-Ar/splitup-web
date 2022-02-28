297f1332f1fc81ff18b38017e67d91b8a742c04c
Nivethan <arnive19@gmail.com>
2022-02-28 08:47:06 +0530

    add more visibility for topbar border

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/self-closing-comp */
 import React from "react";
 import { Link } from "react-router-dom";
 import MusicLogo from "../assets/music.png";
@@ -5,7 +6,7 @@ import MusicLogo from "../assets/music.png";
 function TopBar() {
   return (
     <Link to="/">
-      <div className="flex items-center gap-2 border-b-2 border-opacity-20 pb-3 pt-4 border-gray-400 ">
+      <div className="flex items-center gap-2 border-b-2 border-opacity-40 pb-3 pt-4 border-gray-300 ">
         <img src={MusicLogo} alt="page logo" className="w-10 bg-white rounded-full animate-pulse" />
         <h6 className="font-semibold text-3xl font-poppins text-white tracking-widest text-shadow-md ">
           SPLITUP
